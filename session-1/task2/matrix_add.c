45d9d320d5118156ee843d4eae8bfccb1d81907d
FraserHedger <wnhn0501@leeds.ac.uk>
2026-02-16 12:56:43 +0000

    done most of it

--- session-1/task2/matrix_add.c
+++ session-1/task2/matrix_add.c
@@ -9,12 +9,23 @@
     float a[4][4];
     float b[4][4];
     float c[4][4];
+    for (int i = 0; i<4; i++)
+    {
+      for(int t = 0; t<4; t++)
+      {
+         c[i][t] = a[i][t] + b[i][t];
+         printf("Row %d, Collumn %d = %f, ", i, t, c[i][t]);
 
+      }
+      
+    }
+    
     /*
     Intialise the matrix a and b entries to 1. 
     Write code to compute the sum.
     Store your answer in matrix c
     Print your final answer
     */
+   
     return 0;
  }
