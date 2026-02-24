b7ca75cf26bad55772a8c4951c7b6c699e931ae9
FraserHedger <wnhn0501@leeds.ac.uk>
2026-02-24 16:45:28 +0000

    this is done

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -12,6 +12,17 @@ int main( int argc, char **argv ) {
     // define appropriate data to hold your answer
 
     // process the command-line data using appropriate string functions
+    
+    char *str = argv[1];
+    int len = strlen(str);
+    char buffer[200] = "";
+    for (int i = 0; i < len - 1; i++)
+    {
+        char temp[3] = {str[i], '-', '\0'};
+        strcat(buffer, temp);
+    } 
+
+    strcat(buffer, &str[len - 1]);
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
